8d8d5e271b32322f9ca08b068888ff5b818cf535
AntosDev <antosdev@gmail.com>
2023-02-16 12:38:18 +0200

    fix build issue

--- FilesManagement.Core.Infra/DataAccess/FilesRepository.cs
+++ FilesManagement.Core.Infra/DataAccess/FilesRepository.cs
@@ -1,14 +1,20 @@
-﻿using FilesManagement.Core.Domain.InvertedDependencies;
+﻿using FilesManagement.Core.Domain;
+using FilesManagement.Core.Domain.InvertedDependencies;
 
 namespace FilesManagement.Core.Infra.DataAccess
 {
     internal class FilesRepository : IFilesRepository
     {
-        public async void Save(IEnumerable<Domain.FileDomain> files)
+        public void Save(IEnumerable<Domain.FileDomain> files)
         {
             throw new NotImplementedException();
         }
-        public void Delete(IEnumerable<Domain.FileDomain> files)
+        public FileDomain Find(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IEnumerable<string> ids)
         {
             throw new NotImplementedException();
         }
